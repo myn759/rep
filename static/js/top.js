db11747da296bdddffbbdff55f2f22ab616a24ba
nym <tnomura1322@gmail.com>
2019-12-11 00:42:05 +0900

    ファイル整理、もろもろ

---
+++ static/js/top.js
@@ -0,0 +1,19 @@
+// 起動
+function launch() {
+    alert("おしたぜよ");
+}
+
+// はじめにに切り替え
+function changeReadme() {
+    document.getElementById("messages").innerText = "私を読め";
+}
+
+// お知らせに切り替え
+function changeNotice() {
+    document.getElementById("messages").innerText = "おしらせだよ";
+}
+
+// 注意事項に切り替え
+function changeCaution() {
+    document.getElementById("messages").innerText = "注意です";
+}
